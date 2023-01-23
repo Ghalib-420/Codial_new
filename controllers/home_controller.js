0c5f93aa9f7538f453eefc5ae406f4fccb4b354e
manishksah94@gmail.com <manishksah94@gmail.com>
2023-01-23 23:12:20 +0530

    just checking

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,35 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+  if (req.isAuthenticated()) {
+    Post.findOne({ user: req.user._id }, function (err, posts) {
+      if (err) {
+        return res.render("home", {
+          title: "Home",
+          post: [],
+        });
+      }
+      return res.render("home", {
+        title: "Home",
+        post: posts,
+      });
+    });
+  } else {
+    Post.find({}, function (err, posts) {
+      if (err) {
+        return res.render("home", {
+          title: "Home",
+          post: [],
+        });
+      }
+      return res.render("home", {
+        title: "Home",
+        post: posts,
+      });
+    });
+  }
+
+  // return res.redirect("/users/sign-in");
 };
 
 // module.exports.actionName = function(req, res){}
