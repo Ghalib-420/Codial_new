c973388e754c73906fab39fc6a5b96dad2125a25
manishksah94@gmail.com <manishksah94@gmail.com>
2023-01-28 16:00:07 +0530

    Converting post in Ajax

---
+++ assets/js/home_posts.js
@@ -0,0 +1,73 @@
+{
+  // method to submit the form data for new posts using AJAX
+  let createPost = function () {
+    let newPostForm = $("#new-post-form");
+    newPostForm.submit(function (e) {
+      e.preventDefault();
+
+      $.ajax({
+        type: "post",
+        url: "/post/create-post",
+        data: newPostForm.serialize(),
+        success: function (data) {
+          console.log(data);
+          let newPost = newPostDom(data.data.post);
+          $("#posts-list-container>ul").prepend(newPost);
+          deletePost($(" delete-post-button", newPost));
+        },
+        error: function (error) {
+          console.log(error.responseText);
+        },
+      });
+    });
+  };
+
+  //method to create a post in DOM
+
+  let newPostDom = function (post) {
+    return $(`<li id="post-${post._id}">
+    <p id="content">
+      
+      <small>
+        <a class="delete-post-button" href="/post/destroy/${post._id}">X </a>
+      </small>
+  
+     ${post.content}<br />
+      <small id="date">${post.user.name}</small>
+    </p>
+  
+    <div class="post-comment">
+      <form action="/comment/create/?id=${post._id}" method="POST" required>
+        <input id="cmt" name="content" />
+        <input type="submit" value="post" />
+      </form>
+    </div>
+
+    <div class="post-comment-list">
+  <ul id="post-comment-${post._id}">
+    
+  </ul>
+</div>
+    
+  </li>`);
+  };
+
+  let deletePost = function (deleteLink) {
+    $(deleteLink).click(function (e) {
+      e.preventDefault();
+
+      $.ajax({
+        type: "get",
+        url: $(deleteLink).prop("href"),
+        success: function (data) {
+          $(`#post-${data.data.post_id}`).removed();
+        },
+        error: function (error) {
+          console.log(error.responseText);
+        },
+      });
+    });
+  };
+
+  createPost();
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,17 +3,24 @@ const Comment = require("../models/comment");
 
 module.exports.createPost = async function (req, res) {
   try {
-    if (req.isAuthenticated()) {
-      let post = await Post.create({
-        content: req.body.content,
-        user: req.user._id,
+    let post = await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        massage: "Post created",
       });
-      req.flash("sucess", "Posted");
-      return res.redirect("/");
-    } else {
-      req.flash("alert", "Sign In First");
-      return res.redirect("/users/sign-in");
     }
+    req.flash("sucess", "Posted");
+    return res.redirect("/");
+
+    // req.flash("alert", "Sign In First");
+    // return res.redirect("/users/sign-in");
   } catch (err) {
     console.log("Error", err);
     return res.redirect("back");
@@ -26,7 +33,16 @@ module.exports.destroy = async function (req, res) {
     if (post.user == req.user.id) {
       post.remove();
       req.flash("success", "Deleted");
-      Comment.deleteMany({ post: req.params.id });
+      await Comment.deleteMany({ post: req.params.id });
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post_id: req.params.id,
+          },
+        });
+      }
+
       return res.redirect("back");
     } else {
       return res.redirect("back");
