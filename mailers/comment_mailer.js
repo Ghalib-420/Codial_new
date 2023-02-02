261093bf3026a54ab1d71b4a7aa5e819ca4ed640
manishksah94@gmail.com <manishksah94@gmail.com>
2023-02-02 16:01:35 +0530

    Adding Templet to the mail

--- mailers/comment_mailer.js
+++ mailers/comment_mailer.js
@@ -3,13 +3,16 @@ const nodeMailer = require("../config/nodemailer");
 //this is another way of exporting a method
 
 exports.newComment = (comment) => {
-  console.log("inside newComment mailer");
+  let htmlString = nodeMailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
   nodeMailer.transporter.sendMail(
     {
       from: "mkumardummy270@gmail.com",
       to: comment.user.email,
       subject: "New Comment",
-      html: "<h1>Yup ,Your comment is now publised",
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
